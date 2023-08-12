39e8d176d31cec69375452f4d1664b854d0ce2e0
kishor236jha <74865602+kishor236jha@users.noreply.github.com>
2023-08-12 11:40:57 +0530

    comming-chnge

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SimpleForm from './SimpleForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Simple Form</h1>
+      <SimpleForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/SimpleForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const SimpleForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form data submitted:', formData);
+  };
+
+  return (
+    <div>
+      <h2>Simple Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Message:</label>
+          <textarea
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
